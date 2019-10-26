fc1ff70a2cd4582d700926e63be724657a9bdf27
wachukxs <nwachukwuossai@gmail.com>
2019-10-26 14:53:16 +0100

    updated unit test

--- test/test.js
+++ test/test.js
@@ -14,7 +14,8 @@ describe('Testing all end points', function() {
 
         it('All men 30 years and above earning more than $3,000', function(done) {
             request('http://localhost:8087/allmen' , function(error, response, body) {
-                expect(JSON.parse(body)).instanceOf(Object, 'is an object');  //.to.equal('Hello World');
+                expect(JSON.parse(body)).instanceOf(Object, 'is an object');
+                expect(response.statusCode).to.equal(200);
                 done();
             });
         });
@@ -23,7 +24,9 @@ describe('Testing all end points', function() {
     describe ('Get a person using an id that exists', function() {
         it('status', function(done){
             request('http://localhost:8087/id/5bc9b385d40f6ed3fac74cbe', function(error, response, body) {
-                expect(JSON.parse(body)).instanceOf(Object, 'is an object'); // expect(response.statusCode).to.equal(404);
+                expect(JSON.parse(body)).instanceOf(Object, 'is an object');
+                expect(response.statusCode).to.equal(200);
+                expect(JSON.parse(body)._id).to.equal('5bc9b385d40f6ed3fac74cbe');
                 done();
             });
         });
